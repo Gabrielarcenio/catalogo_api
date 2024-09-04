3ad29456f78caaae9bb98280bcfa2f48c53c8d1d
Gabriel Arcenio <Arcenio25@gmail.com>
2024-09-04 15:36:30 -0300

    Adiciona configuração do Docker para o backend e MongoDB

--- server/server.js
+++ server/server.js
@@ -1,23 +1,24 @@
-// server/server.js
 const express = require('express');
 const mongoose = require('mongoose');
-const apiRoutes = require('./routes/apiRoutes'); // Importa as rotas de APIs
-const authRoutes = require('./routes/authRoutes'); // Importa as rotas de autenticação
-const verifyToken = require('./middleware/authMiddleware'); // Middleware de autenticação
-
+const apiRoutes = require('./routes/apiRoutes');
+const authRoutes = require('./routes/authRoutes');
+const verifyToken = require('./middleware/authMiddleware');
 const swaggerUi = require('swagger-ui-express');
 const swaggerJSDoc = require('swagger-jsdoc');
 
 const app = express();
 const port = 3000;
 
-// Conectar ao MongoDB
-mongoose.connect('mongodb://localhost/catalogo-api', { useNewUrlParser: true, useUnifiedTopology: true })
+// Use a variável de ambiente MONGO_URI para a conexão
+const mongoUri = process.env.MONGO_URI || 'mongodb://localhost:27017/catalogo-api';
+
+mongoose.connect(mongoUri)
   .then(() => console.log('Conectado ao MongoDB'))
   .catch(err => console.error('Erro ao conectar ao MongoDB', err));
 
-// Middleware para analisar JSON
 app.use(express.json());
+app.use('/auth', authRoutes);
+app.use('/apis', verifyToken, apiRoutes);
 
 // Configuração do Swagger
 const swaggerOptions = {
@@ -40,11 +41,4 @@ const swaggerOptions = {
 const swaggerDocs = swaggerJSDoc(swaggerOptions);
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
-// Rotas de Autenticação
-app.use('/auth', authRoutes); // Rota base para autenticação
-
-// Protege a rota de APIs usando middleware de autenticação
-app.use('/apis', verifyToken, apiRoutes); // Rota protegida para APIs
-
-// Inicia o servidor
 app.listen(port, () => console.log(`Servidor rodando na porta ${port}`));
